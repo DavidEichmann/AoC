1adc66540db4f2486a580927e04ad8b0bdc1bfaa
David Eichmann <EichmannD@gmail.com>
2017-12-15 21:30:48 +0000

    Day 4 + 5

--- src/Main.hs
+++ src/Main.hs
@@ -5,10 +5,87 @@ module Main where
 import Safe
 import qualified Data.Map as M
 import Data.Maybe
-import Data.List (tails)
+import Data.List (tails, nub, sort)
+import qualified Data.Vector.Unboxed as V
+import qualified Data.Vector.Unboxed.Mutable as MV
+import Control.Monad.ST
 
 main :: IO ()
-main = aoc2b
+main = aoc5
+
+--
+-- Day 5
+--
+
+aoc5 :: IO ()
+aoc5 = do
+  maze <- mapMaybe readMay . lines <$> readFile "src/input_05"
+  print $ aoc5bCount maze
+
+aoc5aCount :: [Int] -> Int
+aoc5aCount mazeList = runST $ go 0 0 =<< V.unsafeThaw (V.fromList mazeList)
+  where
+    go :: Int -> Int -> MV.MVector m Int -> ST m Int
+    go stepCount index maze
+      | index < 0 || n <= index = return stepCount
+      | otherwise = do
+        offset <- MV.read maze index
+        MV.modify maze (+1) index
+        go (stepCount + 1) (index + offset) maze
+      where
+        n = MV.length maze
+
+aoc5bCount :: [Int] -> Int
+aoc5bCount mazeList = runST $ do
+  initialMaze <- V.unsafeThaw (V.fromList mazeList)
+  let
+    go :: Int -> Int -> MV.MVector m Int -> ST m Int
+    go stepCount index maze
+      | index < 0 || n <= index = return stepCount
+      | otherwise = do
+        offset <- MV.unsafeRead maze index
+        MV.unsafeWrite
+          maze
+          index
+          (if offset >= 3 then offset - 1 else offset + 1)
+        go (stepCount + 1) (index + offset) maze
+      where
+        n = MV.length maze
+  go 0 0 initialMaze
+
+--
+-- Day 4
+--
+
+aoc4b :: IO ()
+aoc4b = do
+  fileStr <- readFile "src/input_04_a"
+  print
+    . length
+    . filter isValid
+    . filter (not . null)
+    . fmap words
+    . lines
+    $ fileStr
+  where
+    isValid :: [String] -> Bool
+    isValid ws = let
+      ws' = fmap sort ws
+      in ws' == nub ws'
+
+aoc4a :: IO ()
+aoc4a = do
+  fileStr <- readFile "src/input_04_a"
+  print
+    . length
+    . filter isValid
+    . filter (not . null)
+    . fmap words
+    . lines
+    $ fileStr
+  where
+    isValid :: [String] -> Bool
+    isValid ws = ws == nub ws
 
 --
 -- Day 3
